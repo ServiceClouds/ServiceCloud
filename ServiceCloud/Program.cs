a26c11f1caaa4b69fcb7b2ee16ff851d1f5176e5
Zukhruf-Ramay <zukhruframay@gmail.com>
2026-07-29 15:53:38 +0500

    swagger without controllers

--- ServiceCloud/Program.cs
+++ ServiceCloud/Program.cs
@@ -12,14 +12,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+
+//swagger implementation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
