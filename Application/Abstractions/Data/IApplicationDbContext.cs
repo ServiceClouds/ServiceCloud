046524d8af17c79a5d81f0579357b61991074eef
Hammad Ahmad <243508@students.au.edu.pk>
2026-07-29 11:30:38 +0500

    done

--- Application/Abstractions/Data/IApplicationDbContext.cs
+++ Application/Abstractions/Data/IApplicationDbContext.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Abstractions.Data
+
 {
-    internal class IApplicationDbContext
-    {
-    }
+    public interface IApplicationDbContext
+{
+    DbSet<T> GetDbSet<T>() where T : class;
+
+
+
+}
 }
