34a8ccedac827ac7062eac2a1160d4009cd2aa90
Hammad Ahmad <243508@students.au.edu.pk>
2026-08-04 21:47:24 +0500

    done

---
+++ ServiceCloud.Web/src/App.jsx
@@ -0,0 +1,7 @@
+import EmailStep from "./component/EmailStep";
+
+function App() {
+  return <EmailStep />;
+}
+
+export default App;
---
+++ ServiceCloud.Web/src/Component/EmailStep.jsx
@@ -0,0 +1,23 @@
+function EmailStep() {
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <label>Email</label>
+
+      <br />
+
+      <input
+        type="email"
+        placeholder="Enter your email"
+      />
+
+      <br />
+      <br />
+
+      <button>Continue</button>
+    </div>
+  );
+}
+
+export default EmailStep;
